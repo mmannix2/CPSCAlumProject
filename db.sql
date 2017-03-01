0cbb0b811f425665f9247b94e4947ceb91e24166
Matt Mannix <mmannix2@mail.umw.edu>
2017-03-01 17:20:18 +0000

    Add volunteers table to db.sql.

--- db.sql
+++ db.sql
@@ -1,10 +1,17 @@
 #CREATE DATABASE db;
 USE db;
 
-#CREATE TABLE jobs (id NOT NULL PRIMARY KEY AUTO_INCREMENT, name CHAR(25), description CHAR(700));
-CREATE TABLE jobs (id NOT NULL PRIMARY KEY, name VARCHAR NOT NULL, companyName VARCHAR NOT NULL, description TEXT(600) NOT NULL);
-INSERT INTO jobs (name, description) VALUES ('Java Programmer', 'Company A', 'You will do things.');
-INSERT INTO jobs (name, description) VALUES ('Unix System Admin', 'Company B', 'You will do different things.');
-INSERT INTO jobs (name, description) VALUES ('Summer Intern', 'Company C', 'You will do more different things.');
+DROP TABLE IF EXISTS jobs;
+    
+CREATE TABLE jobs (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(32) NOT NULL, companyName VARCHAR(32) NOT NULL, description VARCHAR(600) NOT NULL);
+INSERT INTO jobs (name, companyName, description) VALUES ('Java Programmer', 'Company A', 'You will do things.');
+INSERT INTO jobs (name, companyName, description) VALUES ('Unix System Admin', 'Company B', 'You will do different things.');
+INSERT INTO jobs (name, companyName, description) VALUES ('Summer Intern', 'Company C', 'You will do more different things.');
+
+DROP TABLE IF EXISTS volunteers;
+
+CREATE TABLE volunteers (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(32) NOT NULL, email VARCHAR(64) NOT NULL, description VARCHAR(600) NOT NULL);
+INSERT INTO volunteers (name, email, description) VALUES ('John Doe', 'jdoe@company.com', 'I want to teach the students about Hadoop!');
 
 #GRANT ALL ON jobs TO *username*;
+#GRANT ALL ON volunteers TO *username*;
