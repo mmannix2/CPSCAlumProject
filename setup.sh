f6507b42c09e59ab7cdd4fd3054bd3fd0a41a79f
Matt Mannix <mmannix2@mail.umw.edu>
2017-03-17 15:17:51 +0000

    Make more changes to setup.sh.

--- setup.sh
+++ setup.sh
@@ -4,10 +4,11 @@
 
 #Once you do that make this script executable and run it.
 
-sudo mysql < setup/createUsers.sql
+sudo service mysql start
+#sudo mysql < setup/createUsers.sql
 sudo mysql < db.sql
-sudo rm /var/www/html
-sudo ln -s ~/workspace/CPSCDatabase /var/www/html
+sudo rm -r /var/www/html
+sudo ln -s ~/workspace/CPSCDatabase/html /var/www/html
 sudo cp .htaccess /var/www
 sudo cp setup/001-cloud9.conf /etc/apache2/sites-enabled/001-cloud9.conf
 sudo cp setup/apache2.conf /etc/apache2
