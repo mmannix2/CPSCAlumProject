a500bc342464c41f98266b161732feca29257398
Matt Mannix <mmannix2@mail.umw.edu>
2017-03-23 21:50:43 +0000

    Add location, email, and link fields to the db.

--- html/api.php
+++ html/api.php
@@ -1,34 +1,37 @@
 <?php
-//Database connection info
-    $DB_HOST = "127.0.0.1";
-    $DB_USER = "api";
-    $DB_PASSWORD = "password";
-    $DB_NAME = "db";
+/*
+Database connection info
+$DB_HOST = "127.0.0.1";
+$DB_USER = "api";
+$DB_PASSWORD = "password";
+$DB_NAME = "db";
+*/
 
-    $API_KEYS = array("jobs", "volunteers", "announcements");
-
-function checkAPIKey($parts, $API_KEYS) {
-    if(in_array($parts[0], $API_KEYS)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+function connectToDB() {
+    return new PDO("mysql:host=127.0.0.1;dbname=db", "api", "password");
 }
-    
-function loadJobs($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME) {
-    //Connect to DB
+ 
+function loadJobs() {
     try {
-        $db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+        //Connect to DB
+        $db = connectToDB();
         
         //Get Result
-        $results = $db->query('SELECT name, companyName, description FROM jobs')->fetchAll(PDO::FETCH_ASSOC);
+        $results = $db->query('SELECT name, companyName, location, description FROM jobs')->fetchAll(PDO::FETCH_ASSOC);
         $json = json_encode($results);
         return $json;
     }
     catch(PDOException $e) {
         return "<script>console.log(Connection Failed: $e</script>";
     }
+}
+
+function postJob($postInfo) {
+    //TO DO INSERT this new job into the db
+}
+
+function searchJobs() {
+    //TO DO Write this
 }
 
 //Get the request
@@ -38,9 +41,8 @@ $requestParts = explode('/', rtrim($requestURI, '/'));
 switch($_SERVER['REQUEST_METHOD'])
 {
     case 'GET':
-        //TODO do error checking to ensure each api key supports the attempted request
         if( $requestParts[2] == "jobs") {
-            echo loadJobs($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+            echo loadJobs();
         }
         if( $requestParts[2] == "search") {
             $json = json_encode("No results found.");
@@ -49,15 +51,10 @@ switch($_SERVER['REQUEST_METHOD'])
         break;
     case 'POST':
         $request = $_POST['request'];
-        //echo "POST: $request";
         break;
     default:
         //echo "OTHER: $request";
         break;
 }
 
-/*
-TODO Use the data that is parsed out ofthe request to run the correct SQL
-statement on the DB.
-*/
 ?>
